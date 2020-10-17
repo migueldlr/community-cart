1de3fb9c4bc9ddfce11c58eb530ddbe970dbd908
srhollan <srhollan@ncsu.edu>
2020-10-16 22:32:56 -0400

    adding the file for selecting a cart

---
+++ src/components/SelectCart.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+
+import { db } from '../firebase/index';
+
+interface SelectCartProps {
+
+}
+
+const SelectCart = (props: SelectCartProps) => {
+
+}
+
